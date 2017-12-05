c7701f7450672f447daca2c145f6339ea79ea6a7
linh vu <linh.vu@quoine.com>
2017-12-05 10:28:38 +0700

    add eslint

--- src/App/components/InputText/View.jsx
+++ src/App/components/InputText/View.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const Input = styled.input`
   height: 30px;
-  width: 100%;
+  width: calc(100% - 20px);
   margin-top: 5px;
   background-color: #FFFFFF;
   border: 1px solid #e5e6e7;
@@ -14,6 +14,7 @@ const Input = styled.input`
   transition: border-color 0.15s ease-in-out 0s, box-shadow 0.15s ease-in-out 0s;
   font-size: 14px;
   outline: none;
+  padding: 0 10px;
   &:focus {
     border: 1px solid #1ab394;  
   }
